47e016cb9a0c40cfbd1ff636fd2257330ad3e886
Nithish651 <nithish651@gmail.com>
2022-07-13 10:28:51 +0530

    first commi

---
+++ src/codefromscratch/FirstAndLastPosition.java
@@ -0,0 +1,49 @@
+package codefromscratch;
+
+public class FirstAndLastPosition {
+
+	public static int searchHelper(int[] nums, int target, boolean isFirst) {
+		int left = 0;
+		int right = nums.length - 1;
+		int mid = 0;
+		int position = -1;
+
+		while (left <= right) {
+			mid = left + (right - left) / 2;
+
+			if (target < nums[mid])
+				right = mid - 1;
+			else if (target > nums[mid])
+				left = mid + 1;
+			else {
+				position = mid; //possible answer, will be updated if found again
+				//if looking for first position, move to the left
+				if (isFirst)
+					right = mid - 1;
+				else
+					left = mid + 1; //else move to the right
+
+			}
+		}
+		
+		return position;
+	}
+
+	public static int[] searchRange(int[] nums, int target) {
+		int first = searchHelper(nums,target,true);
+		int last = searchHelper(nums,target,false);
+		int [] result = {first,last};
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {5,7,7,8,8,10};
+
+		int[] indices = searchRange(nums, 8);
+
+		for (int i : indices) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
---
+++ src/codefromscratch/SearchAnElement.java
@@ -0,0 +1,29 @@
+package codefromscratch;
+
+public class SearchAnElement {
+
+	public static int searchInSorted(int arr[], int N, int K) 
+	{ 
+	    int left = 0;
+	    int right = N-1;
+	    int mid = 0;
+	    
+	    while(left <= right){
+	        mid = left + ((right - left)/2);
+	        if(arr[mid] == K)
+	            return 1;
+	        else if(K < arr[mid])
+	            right = mid - 1;
+	        else
+	            left = mid + 1;
+	    }
+	       return -1;
+	}
+	
+	
+	public static void main(String[] args) {
+		int [] arr = {1,2,3,4,6};
+		
+		System.out.println(searchInSorted(arr, 5, 6));
+	}
+}
